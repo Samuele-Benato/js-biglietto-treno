6aba40e64e014a5a8940eff8e6b8ee07388a61c4
Samuele-Benato <samuele.benato@gmail.com>
2023-07-19 17:19:13 +0200

    feat : add tiket message

--- js/main.js
+++ js/main.js
@@ -2,24 +2,36 @@
 const kmNumber = parseFloat(prompt("Chilometri da percorrere"));
 console.log("km: ", kmNumber);
 
-// Ask age
-const ageNumber = parseInt(prompt("Età passeggero"));
-if (ageNumber < 0 || kmNumber < 0) {
+if (kmNumber <= 0 || isNaN(kmNumber)) {
   alert("inserisci un numero maggiore di zero");
 } else {
-  console.log("age: ", ageNumber);
+  // Ask age
+  const ageNumber = parseInt(prompt("Età passeggero"));
 
-  // Price
-  const price = kmNumber * 0.21;
-  console.log("prezzo: ", price);
+  if (ageNumber <= 0 || isNaN(ageNumber)) {
+    alert("inserisci un numero maggiore di zero");
+  } else {
+    console.log("age: ", ageNumber);
 
-  // Discounts
-  if (ageNumber < 18) {
-    price -= price * 0.2;
-  } else if (ageNumber > 65) {
-    price -= price * 0.4;
+    // Price
+    let price = kmNumber * 0.21;
+    console.log("prezzo: ", price);
+
+    // Discounts
+    if (ageNumber < 18) {
+      price -= price * 0.2;
+    } else if (ageNumber > 65) {
+      price -= price * 0.4;
+    }
+
+    // Final price
+    const finalPrice = price.toFixed(2);
+
+    // Tiket Message
+    const tiketMessage = ` 
+    <h1 class = "d-inline-block pe-2"> Questo è il preventivo del tuo biglietto!</h1><i class="fs-1 fa-solid fa-face-laugh-beam"></i>
+    <p class = "fs-3"> ${finalPrice} €</p>`;
+
+    document.getElementById("tiketGenerator").innerHTML = tiketMessage;
   }
-
-  // Final price
-  const finalPrice = price.toFixed(2);
 }
